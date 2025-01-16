a34ec0e52219eee8480f36c92e84634677aa0310
Arbaz Arshad <arbazz.arshad10@gmail.com>
2025-01-16 12:08:13 +0000

    new changes

---
+++ working_with_csv.py
@@ -0,0 +1,31 @@
+import csv
+
+with open('data.csv', 'r') as file: #replace your file name
+    csv_reader = csv.reader(file)
+    header = next(csv_reader)  # Read the header
+    for row in csv_reader:
+        # Process each row
+        print(row)
+
+
+#another method by built inn python libraries
+
+import pandas as pd
+
+df = pd.read_csv('data.csv')
+#print(df.head())  # Display first few rows
+
+#write a csv file from a data
+import csv
+
+data = [
+    ['id', 'name', 'age'],
+    [1, 'Alice', 30],
+    [2, 'Bob', 25],
+    [3, 'Cathy', 27]
+]
+
+with open('output.csv', 'w', newline='') as file:
+    csv_writer = csv.writer(file)
+    csv_writer.writerows(data)
+
---
+++ working_with_json.py
@@ -0,0 +1,32 @@
+import json
+'''
+open a json file in r = read mode
+then load the file 
+finally print it
+'''
+with open('data.json', 'r') as file:
+    data = json.load(file)
+print(data)
+
+###########################
+
+import json
+'''
+to write a json file use w
+json.dump ftn
+'''
+python_obj = {'name': 'John', 'age': 30}
+with open('output.json', 'w') as file:
+    json.dump(python_obj, file)
+
+#####
+import json
+'''
+for reading a large json file
+increment it line by line
+'''
+
+with open('large_file.json', 'r') as file:
+    for line in file:
+        data = json.loads(line)
+        # Process each line of JSON data    
